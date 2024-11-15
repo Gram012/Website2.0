32ef352e24e39fae490ce9da6b59a1210486c5e7
Gram012 <gramo012@umn.edu>
2024-11-15 14:25:23 -0600

    Main Text

--- src/Elements/welcome.tsx
+++ src/Elements/welcome.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import "../css/welcome.css";
 
 const Welcome: React.FC = () => {
-  return <div>Some Welcome Texts</div>;
+  return <div className="welcomeText">Some Welcome Texts</div>;
 };
 
 export default Welcome;
